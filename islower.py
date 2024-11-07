a0ebf9caf545d58cf6bd123066e9dd9d130ccb32
Esraa3bdeen <esraaabdyn4@gmail.com>
2024-11-07 14:25:03 +0200

    Update islower.py

--- islower.py
+++ islower.py
@@ -1,13 +1,12 @@
 def islower(c):
-    # Check if c is a single character
+    
     if len(c) != 1:
         raise ValueError("Input must be a single character")
     
-    # Get the Unicode code point of the character
+    
     char_code = ord(c)
     
-    # Check if the character is lowercase
-    # Lowercase letters in Unicode range from 97 ('a') to 122 ('z')
+    
     if 97 <= char_code <= 122:
         return True
     else:
